9d628c58af2fa8064e57d757243829b5e339ae98
Tornado3708 <you@example.com>
2022-06-18 20:05:53 +0100

    addition,substraction,multiplication,division

--- src/Dot.js
+++ src/Dot.js
@@ -13,7 +13,12 @@ module.exports = class Dot{
         this.y = x_y_z[0]["y"]||x_y_z[0][1]||x_y_z[1]||0;
         this.z = x_y_z[0]["z"]||x_y_z[0][2]||x_y_z[2]||0;
         
-        this.__proto__.translate = (x,y,z) => { this.x=x; this.y=y; this.z=z; };
+        this.__proto__.translate = (x=0,y=0,z=0)=>{this.x=x;this.y=y;this.z=z;};
+
+        this.__proto__.add  = (x=0,y=0,z=0) =>{this.x+=x;this.y+=y;this.z+=z;};
+        this.__proto__.sub  = (x=0,y=0,z=0) =>{this.x-=x;this.y-=y;this.z-=z;};
+        this.__proto__.mult = (m=1)=>{this.x*=m;this.y*=m;this.z*=m;};
+        this.__ptoto__.div  = (d=1)=>{this.x*=1/d;this.y*=1/d;this.z*=1/d;};
         
     }
 };
