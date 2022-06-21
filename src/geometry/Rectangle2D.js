a212a18946d8bd47c57307ffb8360ee87ddb008d
Tornado3708 <you@example.com>
2022-06-21 13:13:39 +0100

    rectangle 2d

---
+++ src/geometry/Rectangle2D.js
@@ -0,0 +1,17 @@
+class Rectangle2D{
+    
+  static area(rect){return rect.width*rect.height;};
+
+  static diagonal(rect){
+      return Math.sqrt(rect.width*rect.width+rect.height*rect.height);};
+    
+    constructor(x,y,width,height){
+      this.x=x;this.y=y;
+      this.width=width;
+      this.height=height;
+
+      this.__proto__.oppositeX =()=>{return this.x+this.width;};
+      this.__proto__.oppositeY =()=>{return this.y+this.height;};
+    }
+}
+module.exports = Rectangle2D;
