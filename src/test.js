fcefe7b080ca92ac1d59241bf2b4ccf0d989084a
Tornado3708 <you@example.com>
2022-06-22 12:23:47 +0100

    fixed

--- src/test.js
+++ src/test.js
@@ -1,5 +1,13 @@
 const MathPlus = require("./index.js");
 MathPlus.install();
-const {Vector} = MathPlus;
-const vec = new Vector(1,1,1);
+const {Matrix,Triangle2D,Complex} = MathPlus;
+let complex;
+for (let x = -Math.TAU;x < Math.TAU;x+=0.01){
+        complex = new Complex(x,1);
+        console.log(complex.phi());
+        console.log(complex.zet());
+        console.log(complex.abs());
+    }
+
+//console.log(MathPlus);
 //console.log(Math);
