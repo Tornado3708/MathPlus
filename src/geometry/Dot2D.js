b13a6221cbd8fd845439eb4748c3664a47ef2368
Tornado3708 <you@example.com>
2022-06-27 18:28:09 +0100

    regular commit

--- src/geometry/Dot2D.js
+++ src/geometry/Dot2D.js
@@ -11,7 +11,10 @@ class Dot2D{
       this.x = xy[0]["x"] || xy[0][0] || xy[0] || 0;
       this.y = xy[0]["y"] || xy[0][1] || xy[1] || 0;
 
-      this.__proto__.translate = (x=0,y=0)=>{this.x=x;this.y=y;;};
+      this.__proto__.translate = (x=0,y=0)=>{
+        this.x = x;
+        this.y = y;
+      };
 
       this.__proto__.add  = (x=0,y=0)=>{
         this.x += x;
