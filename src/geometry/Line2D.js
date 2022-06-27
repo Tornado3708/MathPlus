6c155e4aa06ff1df2339c15e1d017fbf91dc535f
Tornado3708 <you@example.com>
2022-06-27 19:15:07 +0100

    translate function

--- src/geometry/Line2D.js
+++ src/geometry/Line2D.js
@@ -12,11 +12,13 @@ class Line2D{
       this.x2 = xy[0]["x2"] || xy[0][2] || xy[2] || 0;
       this.y2 = xy[0]["y2"] || xy[0][3] || xy[3] || 0;
       
-      this.translate = (x1,y1,x2,y2)=>{
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+      this.__proto__ = {};
+      
+      this.__proto__.translate = (...xy)=>{
+        this.x1 = xy[0]["x1"] || xy[0][0] || xy[0] || 0;
+        this.y1 = xy[0]["y1"] || xy[0][1] || xy[1] || 0;
+        this.x2 = xy[0]["x2"] || xy[0][2] || xy[2] || 0;
+        this.y2 = xy[0]["y2"] || xy[0][3] || xy[3] || 0;
       };
       
       this.__proto__.length = () =>{
--- src/geometry/Line3D.js
+++ src/geometry/Line3D.js
@@ -14,7 +14,18 @@ class Line3D{
       this.x2 = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
       this.y2 = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
       this.z2 = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
-        
+      
+      this.__proto__ = {};
+      
+      this.__proto__.translate = function(...xyz){
+        this.x1 = xyz[0]["x1"] || xyz[0][0] || xyz[0] || 0;
+        this.y1 = xyz[0]["y1"] || xyz[0][1] || xyz[1] || 0;
+        this.z1 = xyz[0]["z1"] || xyz[0][2] || xyz[2] || 0;
+        this.x2 = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
+        this.y2 = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
+        this.z2 = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
+      };
+      
       this.__proto__.length = () =>{
         let x = this.x1 - this.x2;
         let y = this.y1 - this.y2;
