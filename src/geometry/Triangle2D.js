a9a4d6e9120509a7eeb5946c15da655cb73a3527
Tornado3708 <you@example.com>
2022-06-27 19:10:10 +0100

    translate function

--- src/geometry/Triangle2D.js
+++ src/geometry/Triangle2D.js
@@ -8,6 +8,17 @@ class Triangle2D{
       this.y[0] = xy[0]["y1"] || xy[0][1] || xy[1] || 0;
       this.y[1] = xy[0]["y2"] || xy[0][3] || xy[3] || 0;
       this.y[2] = xy[0]["y3"] || xy[0][5] || xy[5] || 0;
+      
+      this.__proto__ = {};
+      
+      this.__proto__.translate = function(...xy){
+        this.x[0] = xy[0]["x1"] || xy[0][0] || xy[0] || 0;
+        this.x[1] = xy[0]["x2"] || xy[0][2] || xy[2] || 0;
+        this.x[2] = xy[0]["x3"] || xy[0][4] || xy[4] || 0;
+        this.y[0] = xy[0]["y1"] || xy[0][1] || xy[1] || 0;
+        this.y[1] = xy[0]["y2"] || xy[0][3] || xy[3] || 0;
+        this.y[2] = xy[0]["y3"] || xy[0][5] || xy[5] || 0;
+      };
     }
 }
 
--- src/geometry/Triangle3D.js
+++ src/geometry/Triangle3D.js
@@ -3,15 +3,30 @@ class Triangle3D{
   y = [];
   z = [];
     constructor(...xyz){
-    this.x[0] = xyz[0]["x1"] || xyz[0][0] || xyz[0] || 0;
-    this.x[1] = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
-    this.x[2] = xyz[0]["x3"] || xyz[0][6] || xyz[6] || 0;
-    this.y[0] = xyz[0]["y1"] || xyz[0][1] || xyz[1] || 0;
-    this.y[1] = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
-    this.y[2] = xyz[0]["y3"] || xyz[0][7] || xyz[7] || 0;
-    this.z[0] = xyz[0]["z1"] || xyz[0][2] || xyz[2] || 0;
-    this.z[1] = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
-    this.z[2] = xyz[0]["z3"] || xyz[0][8] || xyz[8] || 0;
+    
+      this.x[0] = xyz[0]["x1"] || xyz[0][0] || xyz[0] || 0;
+      this.x[1] = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
+      this.x[2] = xyz[0]["x3"] || xyz[0][6] || xyz[6] || 0;
+      this.y[0] = xyz[0]["y1"] || xyz[0][1] || xyz[1] || 0;
+      this.y[1] = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
+      this.y[2] = xyz[0]["y3"] || xyz[0][7] || xyz[7] || 0;
+      this.z[0] = xyz[0]["z1"] || xyz[0][2] || xyz[2] || 0;
+      this.z[1] = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
+      this.z[2] = xyz[0]["z3"] || xyz[0][8] || xyz[8] || 0;
+    
+      this.__proto__ = {};
+    
+      this.__proto__.translate = function(...xyz){
+        this.x[0] = xyz[0]["x1"] || xyz[0][0] || xyz[0] || 0;
+        this.x[1] = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
+        this.x[2] = xyz[0]["x3"] || xyz[0][6] || xyz[6] || 0;
+        this.y[0] = xyz[0]["y1"] || xyz[0][1] || xyz[1] || 0;
+        this.y[1] = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
+        this.y[2] = xyz[0]["y3"] || xyz[0][7] || xyz[7] || 0;
+        this.z[0] = xyz[0]["z1"] || xyz[0][2] || xyz[2] || 0;
+        this.z[1] = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
+        this.z[2] = xyz[0]["z3"] || xyz[0][8] || xyz[8] || 0;
+      };
   }
 }
 
