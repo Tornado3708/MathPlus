2a259e86da937098f48428893012772abad2501b
Tornado3708 <you@example.com>
2022-06-27 18:35:24 +0100

    regular commit

--- src/geometry/Line2D.js
+++ src/geometry/Line2D.js
@@ -1,20 +1,28 @@
 class Line2D{
   static length(line){
-    let x = line.x1-line.x2; let y = line.y1-line.y2;
-    return Math.sqrt(x*x+y*y);
+    let x = line.x1 - line.x2;
+    let y = line.y1 - line.y2;
+    return Math.sqrt(x * x + y * y);
   }
 
-    constructor(...x_y){
+    constructor(...xy){
 
-      this.x1 = x_y[0]["x1"]||x_y[0][0]||x_y[0]||0;
-      this.y1 = x_y[0]["y1"]||x_y[0][1]||x_y[1]||0;
-      this.x2 = x_y[0]["x2"]||x_y[0][2]||x_y[2]||0;
-      this.y2 = x_y[0]["y2"]||x_y[0][3]||x_y[3]||0;
-        
+      this.x1 = xy[0]["x1"] || xy[0][0] || xy[0] || 0;
+      this.y1 = xy[0]["y1"] || xy[0][1] || xy[1] || 0;
+      this.x2 = xy[0]["x2"] || xy[0][2] || xy[2] || 0;
+      this.y2 = xy[0]["y2"] || xy[0][3] || xy[3] || 0;
+      
+      this.translate = (x1,y1,x2,y2)=>{
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+      };
+      
       this.__proto__.length = () =>{
-        let x = this.x1-this.x2;
-        let y = this.y1-this.y2;
-        return Math.sqrt(x*x+y*y);
+        let x = this.x1 - this.x2;
+        let y = this.y1 - this.y2;
+        return Math.sqrt(x * x + y * y);
       };
     }
 }
--- src/geometry/Line3D.js
+++ src/geometry/Line3D.js
@@ -1,23 +1,25 @@
 class Line3D{
   static length(line){
-    let x = line.x1-line.x2; let y = line.y1-line.y2;let z = line.z1-line.z2;
-    return Math.sqrt(x*x+y*y+z*z);
+    let x = line.x1 - line.x2;
+    let y = line.y1 - line.y2;
+    let z = line.z1 - line.z2;
+    return Math.sqrt(x * x + y * y + z * z);
   }
 
     constructor(...xyz){
 
-      this.x1 = xyz[0]["x1"]||xyz[0][0]||xyz[0]||0;
-      this.y1 = xyz[0]["y1"]||xyz[0][1]||xyz[1]||0;
-      this.z1 = xyz[0]["z1"]||xyz[0][2]||xyz[2]||0;
-      this.x2 = xyz[0]["x2"]||xyz[0][3]||xyz[3]||0;
-      this.y2 = xyz[0]["y2"]||xyz[0][4]||xyz[4]||0;
-      this.z2 = xyz[0]["z2"]||xyz[0][5]||xyz[5]||0;
+      this.x1 = xyz[0]["x1"] || xyz[0][0] || xyz[0] || 0;
+      this.y1 = xyz[0]["y1"] || xyz[0][1] || xyz[1] || 0;
+      this.z1 = xyz[0]["z1"] || xyz[0][2] || xyz[2] || 0;
+      this.x2 = xyz[0]["x2"] || xyz[0][3] || xyz[3] || 0;
+      this.y2 = xyz[0]["y2"] || xyz[0][4] || xyz[4] || 0;
+      this.z2 = xyz[0]["z2"] || xyz[0][5] || xyz[5] || 0;
         
       this.__proto__.length = () =>{
-        let x = this.x1-this.x2;
-        let y = this.y1-this.y2;
-        let z = this.z1-this.z2;
-        return Math.sqrt(x*x+y*y+z*z);
+        let x = this.x1 - this.x2;
+        let y = this.y1 - this.y2;
+        let z = this.z1 - this.z2;
+        return Math.sqrt(x * x + y * y + z * z);
       };
     }
 }
