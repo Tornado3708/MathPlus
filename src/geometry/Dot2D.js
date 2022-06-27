b7cdb00ae976fa02ad07113da01c56c4666d0b2d
Tornado3708 <you@example.com>
2022-06-27 18:26:47 +0100

    regular commit

--- src/geometry/Dot2D.js
+++ src/geometry/Dot2D.js
@@ -1,23 +1,39 @@
 class Dot2D{
     
     static distance(dot_a,dot_b){
-        let x = dot_a.x-dot_b.x;
-        let y = dot_a.y-dot_b.y;
-        return Math.sqrt(x*x+y*y);
+        let x = dot_a.x - dot_b.x;
+        let y = dot_a.y - dot_b.y;
+        return Math.sqrt(x * x + y * y);
     };
     
     constructor(...xy){
         
-        this.x = xy[0]["x"]||xy[0][0]||xy[0]||0;
-        this.y = xy[0]["y"]||xy[0][1]||xy[1]||0;
-        
-        this.__proto__.translate = (x=0,y=0)=>{this.x=x;this.y=y;;};
+      this.x = xy[0]["x"] || xy[0][0] || xy[0] || 0;
+      this.y = xy[0]["y"] || xy[0][1] || xy[1] || 0;
 
-        this.__proto__.add  = (x=0,y=0) =>{this.x+=x;this.y+=y;};
-        this.__proto__.sub  = (x=0,y=0) =>{this.x-=x;this.y-=y;};
-        this.__proto__.mult = (m=1)=>{this.x*=m;this.y*=m;};
-        this.__ptoto__.div  = (d=1)=>{this.x*=1/d;this.y*=1/d;};
+      this.__proto__.translate = (x=0,y=0)=>{this.x=x;this.y=y;;};
+
+      this.__proto__.add  = (x=0,y=0)=>{
+        this.x += x;
+        this.y += y;
+      };
+      
+      this.__proto__.sub  = (x=0,y=0)=>{
+        this.x -= x;
+        this.y -= y;
+      };
+      
+      this.__proto__.mult = (m=1)=>{
+        this.x *= m;
+        this.y *= m;
+      };
+      
+      this.__ptoto__.div  = (d=1)=>{
+        this.x *= 1 / d;
+        this.y *=  1/ d;
+      };
         
     }
 };
+
 module.exports = Dot2D;
--- src/geometry/Dot3D.js
+++ src/geometry/Dot3D.js
@@ -1,25 +1,48 @@
 class Dot3D{
     
     static distance(dot_a,dot_b){
-        let x = dot_a.x-dot_b.x;
-        let y = dot_a.y-dot_b.y;
-        let z = dot_a.z-dot_b.z;
-        return Math.sqrt(x*x+y*y+z*z);
+      let x = dot_a.x - dot_b.x;
+      let y = dot_a.y - dot_b.y;
+      let z = dot_a.z - dot_b.z;
+      return Math.sqrt(x * x + y * y + z * z);
     };
     
     constructor(...xyz){
         
-        this.x = xyz[0]["x"]||xyz[0][0]||xyz[0]||0;
-        this.y = xyz[0]["y"]||xyz[0][1]||xyz[1]||0;
-        this.z = xyz[0]["z"]||xyz[0][2]||xyz[2]||0;
+      this.x = xyz[0]["x"] || xyz[0][0] || xyz[0] || 0;
+      this.y = xyz[0]["y"] || xyz[0][1] || xyz[1] || 0;
+      this.z = xyz[0]["z"] || xyz[0][2] || xyz[2] || 0;
         
-        this.__proto__.translate = (x=0,y=0,z=0)=>{this.x=x;this.y=y;this.z=z;};
+      this.__proto__.translate = (x=0,y=0,z=0)=>{
+        this.x = x;
+        this.y = y;
+        this.z = z;
+      };
 
-        this.__proto__.add  = (x=0,y=0,z=0)=>{this.x+=x;this.y+=y;this.z+=z;};
-        this.__proto__.sub  = (x=0,y=0,z=0)=>{this.x-=x;this.y-=y;this.z-=z;};
-        this.__proto__.mult = (m=1)=>{this.x*=m;this.y*=m;this.z*=m;};
-        this.__ptoto__.div  = (d=1)=>{this.x*=1/d;this.y*=1/d;this.z*=1/d;};
+      this.__proto__.add = (x=0,y=0,z=0)=>{
+        this.x += x;
+        this.y += y;
+        this.z += z;
+      };
         
+      this.__proto__.sub  = (x=0,y=0,z=0)=>{
+        this.x -= x;
+        this.y -= y;
+        this.z -= z;
+      };
+      
+      this.__proto__.mult = (m=1)=>{
+        this.x *= m;
+        this.y *= m;
+        this.z *= m;
+      };
+      
+      this.__ptoto__.div  = (d=1)=>{
+        this.x *= 1 / d;
+        this.y *= 1 / d;
+        this.z *= 1 / d;
+      };
     }
 };
+
 module.exports = Dot3D;
